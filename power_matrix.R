7ca3be7dd9c53be0f1bcea54702caa4b4decc45e
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2021-09-07 13:40:23 +0200

    add first version

---
+++ power_matrix.R
@@ -0,0 +1,40 @@
+library(RnaSeqSampleSize)
+library(heatmap3)
+
+args <- commandArgs(trailingOnly = TRUE)
+print(args)
+mode <- args[1] # data, tcga or none
+m <- as.numeric(args[2]) # number of genes
+m1 <- as.numeric(args[3]) # expected number of DE genes
+n <- as.numeric(args[4]) # sample size
+#rho <- as.numeric(args[3]) # minimum detectable log fold change
+#lambda0 <- as.numeric(args[5]) # avg. reads
+main = "header" # needed, otherwise 'm' screws up the results
+
+if(mode=="none") {
+  phi0 <- as.numeric(args[5]) # dispersion
+  lambda0 <- as.numeric(args[6]) # avg. read count/gene
+  result_file <- args[7]
+  result<-optimize_parameter(fun=est_power,main=main,opt1="rho",opt2="f",opt1Value=c(1.1,2,3,4),opt2Value=c(0.01,0.03,0.05,0.1,0.2),m=m,m1=m1,n=n,phi0=phi0,lambda0=lambda0)
+}
+if(mode=="file") {
+countsPath <- args[5]
+tab = read.table(counts_file_path, header=TRUE, sep="\t")
+counts <- as.matrix(tab[-1,-1])
+dim(counts)
+distrObject <- est_count_dispersion(counts)
+testGenes = min(1000,nrow(counts))
+}
+if(mode=="tcga") {
+  testGenes = 1000
+  distrObject <- args[5]
+  #data(list = distrObject)
+}
+if(mode=="tcga" || mode=="file") {
+  result_file <- args[6]
+  result <- optimize_parameter(fun=est_power_distribution,main=main,opt1="rho",opt2="f",opt1Value=c(1.1,2,3),opt2Value=c(0.01,0.03,0.05,0.1,0.2),m=m, m1=m1,n=n,distributionObject=distrObject)
+}
+print(result)
+pdf(result_file) 
+heatmap3(result, Colv = NA, Rowv = NA, xlab = "Minimum detectable log fold change", ylab = "False discovery rate", scale = "n", col = matlab::jet.colors(1000), cexCol = 1, cexRow = 1, lasCol = 1, lasRow = 1, main = "Power (sensitivity)")
+dev.off()
---
+++ script.py
@@ -0,0 +1,136 @@
+import sys, subprocess, os, configparser
+
+POSTMAN_CONFIG_PATH = "config.txt"
+POSTMAN_USER_CONFIG_PATH = "userconfig.txt"
+RAW_COUNT_FILE = "raw.read.counts.tsv"
+DATA_PATH = "bis_data"
+USAGE = '''USAGE
+
+[user] [sampleCode] [app = power/samples] [app parameters...]
+
+power estimation parameters:
+power none [# genes] [# diff. expr. genes] [replicates (sample size)] [FDR] [dispersion]
+OR
+power tcga [# genes] [# diff. expr. genes] [replicates (sample size)] [FDR] [TCGA name]
+OR
+power data [# genes] [# diff. expr. genes] [replicates (sample size)] [FDR] [code of pilot dataset]
+
+sample size estimation:
+samples none [# genes] [# diff expr. genes] [FDR] [dispersion] [avg counts/gene]
+OR
+samples tcga [# genes] [# diff expr. genes] [FDR] [TCGA name]
+OR
+samples data [# genes] [# diff expr. genes] [FDR] [code of pilot dataset]'''
+
+def fetchData(app, arguments):
+	if(app=="power"):
+		code = arguments[5]
+	if(app=="samples"):
+		code = arguments[4]
+	filePath = os.path.join(DATA_PATH, RAW_COUNT_FILE)
+	arguments = [a.replace(code, filePath) for a in arguments]
+	if not os.path.exists(DATA_PATH):
+		os.mkdir(DATA_PATH)
+	cpar = configparser.RawConfigParser()
+	cpar.read(POSTMAN_USER_CONFIG_PATH)
+	pm_user = cpar.get('postman login','user')
+	pm_pw = cpar.get('postman login','password')
+	cmd = ["java", "-jar", "/postman-cli-0.3.0-custom.jar", code, '@'+POSTMAN_CONFIG_PATH, "-u", pm_user, "-p", pm_pw]
+	print(cmd)
+	try:
+		p = subprocess.Popen(cmd, stdout = subprocess.PIPE)
+		p.wait()
+		(result, error) = p.communicate()
+	except subprocess.CalledProcessError as e:
+		sys.stderr.write("common::run_command() : [ERROR]: output = %s, error code = %s\n" % (e.output, e.returncode))
+	print(result)
+	print(error)
+	
+if len(sys.argv) < 4:
+	print(USAGE)
+	exit()
+user = sys.argv[1]
+sampleCode = sys.argv[2]
+project = sampleCode[0:5]
+app = sys.argv[3]
+arguments = sys.argv[4:]
+source = arguments[0]
+
+if source=="data":
+	fetchData(app, arguments)
+if app=="power":
+	filename = "power.pdf"
+	cmd = ["Rscript", "/power_matrix.R"] + arguments
+elif app=="samples":
+	filename = "sample_size.pdf"
+	cmd = ["Rscript", "/sample_size_matrix.R"] + arguments
+else:
+	print(USAGE)
+	exit()
+cmd += [filename]
+print(cmd)
+
+try:
+	p = subprocess.Popen(cmd, stdout = subprocess.PIPE)
+	p.wait()
+	(result, error) = p.communicate()
+except subprocess.CalledProcessError as e:
+	sys.stderr.write("common::run_command() : [ERROR]: output = %s, error code = %s\n" % (e.output, e.returncode))
+
+print(result)
+print(error)
+
+#create results folder
+results_path = "results"
+if not os.path.exists(results_path):
+	os.mkdir(results_path)
+
+cmd = ["attachi", "-o", results_path, "-u", user, "-t", "Information", project, filename, "RnaSeqSampleSize analysis for "+project]
+print(cmd)
+
+try:
+	p = subprocess.Popen(cmd, stdout = subprocess.PIPE)
+	p.wait()
+	(result, error) = p.communicate()
+except subprocess.CalledProcessError as e:
+	sys.stderr.write("common::run_command() : [ERROR]: output = %s, error code = %s\n" % (e.output, e.returncode))
+
+print(result)
+print(error)
+
+listed_dir = os.listdir(results_path)
+if len(listed_dir) == 1:
+	upload_folder = listed_dir[0]
+else:
+	sys.exit("folder contains "+len(listed_dir)+" files. existing.")
+
+old_results_path = os.path.join(results_path, upload_folder)
+attachCode = project+"000"
+
+cmd = ["sed", "-i", "s/"+attachCode+"/"+sampleCode+"/g", os.path.join(old_results_path, "metadata.txt")]
+print(cmd)
+
+try:
+	p = subprocess.Popen(cmd, stdout = subprocess.PIPE)
+	p.wait()
+	(result, error) = p.communicate()
+except subprocess.CalledProcessError as e:
+	sys.stderr.write("common::run_command() : [ERROR]: output = %s, error code = %s\n" % (e.output, e.returncode))
+
+print(result)
+print(error)
+os.rename(old_results_path, upload_folder)
+
+tar_cmd = ["tar", "-c", upload_folder]
+dync_cmd = ["dync", "-n", upload_folder+".tar", "-k", "untar:True", "data.local"]
+
+try:
+	tar = subprocess.Popen(tar_cmd, stdout=subprocess.PIPE)
+	p = subprocess.Popen(dync_cmd, stdout = subprocess.PIPE, stdin=tar.stdout)
+	p.wait()
+	(result, error) = p.communicate()
+except subprocess.CalledProcessError as e:
+	sys.stderr.write("common::run_command() : [ERROR]: output = %s, error code = %s\n" % (e.output, e.returncode))
+
+print(result)
+print(error)
